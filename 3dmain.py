f3e48fa157374f289945ceb94ad09111a8896a03
Vincent Turner <vincent.turner@jessup.edu>
2023-11-16 12:13:16 -0800

    UI update

--- 3dmain.py
+++ 3dmain.py
@@ -7,9 +7,14 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 #from SlowCube import SlowCube
+from SlowTriangle import SlowTriangle
 import Cube
 import Pieces
 import Border
+
+#Need to install for UI
+from freetype import *
+import pygame.freetype
 
 pygame.init()
 size = width, height = 640, 750
@@ -30,6 +35,10 @@ Cube.Init()
 #cube = Cube.Cube()
 #cube = SlowCube()
 
+#UI
+triangle = SlowTriangle()
+#UI
+
 tetrisPieces = Pieces.createTetrisPieces()
 #print("NUM TETRIS PIECES: " + str(len(tetrisPieces)))
 
@@ -39,14 +48,64 @@ for piece in tetrisPieces:
 
 #print("NUMBER OF CUBES: " + str(tetrisCubes))
 
+
+
+#UI
+
+# Font settings for rendering text
+font_path = "font/Freedom-10eM.ttf"  # Replace with your font file path
+
+def render_text(text, x, y, font_size):
+    """ Renders text onto the screen """
+    font = pygame.freetype.Font(font_path, font_size)
+    text_surface, _ = font.render(text, (255, 255, 255))
+    text_data = pygame.image.tostring(text_surface, "RGBA", True)
+
+    text_texture = glGenTextures(1)
+    glBindTexture(GL_TEXTURE_2D, text_texture)
+    # Set texture parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    # Create texture
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, text_surface.get_width(), text_surface.get_height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, text_data)
+
+    # Render the text as a texture
+    glEnable(GL_TEXTURE_2D)
+    glBindTexture(GL_TEXTURE_2D, text_texture)
+    glBegin(GL_QUADS)
+    # Map the text texture onto a quad
+    glTexCoord2f(0, 1); glVertex2f(x, y)
+    glTexCoord2f(1, 1); glVertex2f(x + text_surface.get_width(), y)
+    glTexCoord2f(1, 0); glVertex2f(x + text_surface.get_width(), y + text_surface.get_height())
+    glTexCoord2f(0, 0); glVertex2f(x, y + text_surface.get_height())
+    glEnd()
+    glDisable(GL_TEXTURE_2D)
+
+    # Clean up the texture
+    #glDeleteTextures(text_texture)
+
+#UI
+
+
+
+
+
+
+
+
 def Update(deltaTime):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             return False
-        
+
     #cube.Update(deltaTime)
     for piece in tetrisPieces:
         piece.Update(deltaTime)
+
+
+    #UI
+    triangle.Update(deltaTime)
+    #UI
 
     return True
 
@@ -56,6 +115,30 @@ def Render():
     #cube.Render()
     for piece in tetrisPieces:
         piece.Render()
+
+    #UI
+
+    # Setting up orthographic projection for text rendering
+    glMatrixMode(GL_PROJECTION)
+    glPushMatrix()
+    glLoadIdentity()
+    gluOrtho2D(0, width, height, 0)
+    glMatrixMode(GL_MODELVIEW)
+    glPushMatrix()
+    glLoadIdentity()
+    
+    # Render the text
+    render_text("Hello World", 10, 10, 48)
+
+    # Restore the previous projection and modelview matrices
+    glPopMatrix()
+    glMatrixMode(GL_PROJECTION)
+    glPopMatrix()
+    glMatrixMode(GL_MODELVIEW)
+
+
+    triangle.Render()
+    #UI
 
     Border.Render()
 
