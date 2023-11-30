a80e188be52f872e35303dbf032b451369921be8
Vincent Turner <vincent.turner@jessup.edu>
2023-11-30 12:35:46 -0800

    Duplicate checker

--- GamePlay.py
+++ GamePlay.py
@@ -106,7 +106,11 @@ def Update(deltaTime, pieces):
         #NEW:
         #Update index to next index and grab a new next index
         index = nextIndex
-        nextIndex = random.randint(0, 6)
+        #Temp: Make sure same piece isn't selected twice (need better solution)
+        while nextIndex == index:
+            print(index)
+            print(nextIndex)
+            nextIndex = random.randint(0, 6)
         #NEW
         #index = random.randint(0, 6)
         OnStart = True
